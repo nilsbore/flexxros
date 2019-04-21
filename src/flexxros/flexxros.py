6698574113010acc6157e27c98cc01fe392ce84a
Nils Bore <nbore@kth.se>
2019-04-21 20:58:25 +0200

    CLeaned up a lot of comments

--- src/flexxros/flexxros.py
+++ src/flexxros/flexxros.py
@@ -2,7 +2,6 @@
 from flexx import flx
 import rospy
 from .external.rospy_message_converter.src.rospy_message_converter import message_converter
-#from external.rospy_message_converter.src.rospy_message_converter import json_message_converter
 import importlib
 import asyncio
 import threading
@@ -10,31 +9,16 @@ import tornado.platform.asyncio as torasync
 import dynamic_reconfigure.client
 import actionlib
 
-#class ROSRelay(flx.Component):
-#
-#    subscribers = flx.DictProp()
-#    publishers = flx.DictProp()
-#    reconfig_clients = flx.DictProp()
-#
-#    def init(self):
-#        pass
-#
-#relay = ROSRelay()
-
-#class ROSPublisher(flx.PyComponent):
 class ROSPublisher:
 
     def __init__(self, topic, topic_type):
-    #def init(self, topic, topic_type):
 
         self.topic_type = topic_type
         self.type = ROSNode.get_type_from_name(topic_type)
         self.pub = rospy.Publisher(topic, self.type, queue_size=10)
 
-#class ROSSubscriber(flx.PyComponent):
 class ROSSubscriber:
 
-    #@relay.reaction('_pitch_feedback')
     def _callback(self, *events):
         topic = self.topic.replace("/", "_")
         for ev in events:
@@ -44,23 +28,17 @@ class ROSSubscriber:
         
         msg_dict = message_converter.convert_ros_message_to_dictionary(msg)
         topic = self.topic.replace("/", "_")
-        #print("Got callback, emitting pitch")
         try:
             self.parent.emit(topic, msg_dict)
-            #relay.emit(topic, msg_dict)
-            #relay._pitch_feedback(msg_dict)
-            #relay.topic_published(topic, msg_dict)
         except BufferError:
             pass
             #print("Caught a buffer error!")
 
     def __init__(self, parent, topic, topic_type):
-    #def init(self, parent, topic, topic_type):
 
         self.parent = parent
         self.topic = topic
         self.sub = rospy.Subscriber(topic, ROSNode.get_type_from_name(topic_type), self.callback)
-        #self.react = relay.reaction(self._callback, topic.replace("/", "_"))
 
 class ROSDynReconfig:
 
@@ -75,10 +53,8 @@ class ROSDynReconfig:
         server_name = self.server_name.replace("/", "_")
         try:
             self.parent.emit(server_name, self.last_config)
-            #relay.emit(server_name, self.last_config)
         except BufferError:
             print("Got buffer error in dyn reconfig")
-        ##    pass
 
     def __init__(self, parent, server_name):
         self.last_config = None
@@ -106,12 +82,6 @@ class ROSActionClient:
         self.parent.emit(self.server_name.replace("/", "_")+"_done", msg_dict)
 
     def send_goal(self, msg_dict):
-        #msg = message_converter.convert_dictionary_to_ros_message(self.type, msg_dict)
-        #msg = json_message_converter.convert_json_to_ros_message(self.type, msg_dict)
-        #goal_type = ROSNode.get_type_from_name(self.server_type+"Result") #"Goal")
-        #print(goal_type)
-        #gg = goal_type()
-        #print(gg)
         msg = message_converter.convert_dictionary_to_ros_message(self.server_type+"Goal", {})
         print("Waiting for action server for 5s")
         self.client.wait_for_server(rospy.Duration.from_sec(5))
@@ -120,11 +90,10 @@ class ROSActionClient:
 
 class ROSNode(flx.PyComponent):
 
-    publishers = {} # flx.DictProp()
+    publishers = {}
     action_clients = {}
-    reconfig_clients = [] # flx.DictProp()
-    subscribers = [] #flx.ListProp()
-    #subscribers = flx.DictProp()
+    reconfig_clients = []
+    subscribers = []
 
     def get_type_from_name(topic_type):
 
@@ -138,51 +107,25 @@ class ROSNode(flx.PyComponent):
 
         return getattr(module, class_str)
 
-    #@flx.reaction("relay.topic_published")
-    #def topic_published(self, topic, value):
-    #    self.emit(topic, value)
-
     @flx.action
     def subscribe(self, topic, topic_type):
 
-        #print("Trying to subscribe to ", topic, " previous subscribers: ", relay.subscribers)
-
         # TODO: needs catching
-        #with self:
         self.subscribers.append(ROSSubscriber(self, topic, topic_type))
-        #if topic not in self.subscribers:
-        #if topic not in relay.subscribers:
-        #    with self:
-        #        flx.mutate_dict(self.subscribers, {'mutation': 'insert', 'objects': {topic: ROSSubscriber(self, topic, topic_type)}})
-        #    flx.mutate_dict(relay.subscribers, {'mutation': 'insert', 'objects': {topic: ""}})
-
 
     @flx.action
     def announce_publish(self, topic, topic_type):
 
-        # this function actually seems superfluous
-        # maybe just check in publish
-        #with self:
         self.publishers[topic] = ROSPublisher(topic, topic_type)
 
     @flx.action
     def announce_action_client(self, server_name, server_type):
 
-        # this function actually seems superfluous
-        # maybe just check in publish
-        #with self:
         self.action_clients[server_name] = ROSActionClient(self, server_name, server_type)
 
     @flx.action
     def announce_reconfig(self, server_name):
         self.reconfig_clients.append(ROSDynReconfig(self, server_name))
-        #print("Reconfig clients: ", relay.reconfig_clients)
-        #if server_name not in self.reconfig_clients:
-        #    print("Adding new reconfig: ", server_name)
-        #    flx.mutate_dict(self.reconfig_clients, {'mutation': 'insert', 'objects': {server_name: ROSDynReconfig(self, server_name)}})
-        #    flx.mutate_dict(relay.reconfig_clients, {'mutation': 'insert', 'objects': {server_name: ""}})
-        #else:
-        #    print("Not add reconfig since already here: ", server_name)
 
     @flx.action
     def set_config(self, server_name, config):
@@ -206,6 +149,12 @@ class ROSWidget(flx.Widget):
 
     def init(self):
         pass
+
+    def announce_publish(self, topic, topic_type):
+        self.root.announce_publish(topic, topic_type)
+
+    def publish(self, topic, data):
+        self.root.publish(topic, data)
 
     def subscribe(self, topic, topic_type, cb):
         self.root.subscribe(topic, topic_type)
@@ -253,7 +202,6 @@ class ROSTopicPlotter(ROSWidget):
         
         # cpu data
         values = list(self.plot.ydata)
-        #values.append(msg.data)
         values.append(msg[self.key])
         values = values[-self.nsamples:]
         self.plot.set_data(times, values)
@@ -263,8 +211,6 @@ class ROSTopicPlotter(ROSWidget):
 
 class ROSDynReconfigWidget(flx.Widget):
 
-    #is_init = flx.BoolProp(False)
-        
     def init(self, server_name):
 
         self.is_init = False
@@ -274,8 +220,6 @@ class ROSDynReconfigWidget(flx.Widget):
             self.vbox = flx.FormLayout(flex=1)
 
         self.root.announce_reconfig(server_name)
-        #node.announce_reconfig(server_name)
-        #self.react.reconnect(0)
 
     @flx.action
     def add_children(self, ev):
@@ -283,7 +227,6 @@ class ROSDynReconfigWidget(flx.Widget):
         print("Got add children!")
 
         if self.is_init:
-            #return
             for c in ev:
                 for child in self.vbox.children:
                     print(child.title, child.text)
@@ -296,14 +239,12 @@ class ROSDynReconfigWidget(flx.Widget):
 
         with self.vbox:
             for c in ev:
-                #print(c, ev[c])
                 if c in ["groups", "type", "source"]:
                     continue
                 l = flx.LineEdit(title=c, text=str(ev[c]))
             flx.Button(text="Set")
             flx.Widget(minsize=60)
 
-        #self._mutate_is_init(True)
         self.is_init = True
 
     def _callback(self, *events):
