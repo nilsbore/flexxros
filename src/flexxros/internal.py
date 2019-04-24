d4725032cbd6702cb07e7eb9ee390f2183f5dec4
Nils Bore <nbore@kth.se>
2019-04-24 11:35:51 +0200

    Split implementation details up into its own library

---
+++ src/flexxros/internal.py
@@ -0,0 +1,110 @@
+import rospy
+from .external.rospy_message_converter.src.rospy_message_converter import message_converter
+import importlib
+import actionlib
+import dynamic_reconfigure.client
+
+def get_type_from_name(topic_type):
+
+    # this code comes from the mongodb_store package
+    parts = topic_type.split('/')
+    cls_string = "%s.msg._%s.%s" % (parts[0], parts[1], parts[1])
+    class_data = cls_string.split(".")
+    module_path = ".".join(class_data[:-1])
+    class_str = class_data[-1]
+    module = importlib.import_module(module_path)
+
+    return getattr(module, class_str)
+
+class ROSPublisher:
+    """
+    Internal class to handle publishers
+    """
+
+    def __init__(self, topic, topic_type):
+
+        self.topic_type = topic_type
+        self.type = get_type_from_name(topic_type)
+        self.pub = rospy.Publisher(topic, self.type, queue_size=10)
+
+class ROSSubscriber:
+    """
+    Internal class to handle subscribers
+    """
+
+    def _callback(self, *events):
+        topic = self.topic.replace("/", "_")
+        for ev in events:
+            self.parent.emit(topic, ev)
+
+    def callback(self, msg):
+        
+        msg_dict = message_converter.convert_ros_message_to_dictionary(msg)
+        topic = self.topic.replace("/", "_")
+        try:
+            self.parent.emit(topic, msg_dict)
+        except BufferError:
+            pass
+            #print("Caught a buffer error!")
+
+    def __init__(self, parent, topic, topic_type):
+
+        self.parent = parent
+        self.topic = topic
+        self.sub = rospy.Subscriber(topic, get_type_from_name(topic_type), self.callback)
+
+class ROSDynReconfig:
+    """
+    Internal class to handle dynamic reconfigure
+    """
+
+    def set_config(self):
+        if self.last_config is not None:
+            self.client.update_configuration(self.last_config)
+
+    def callback(self, config):
+
+        print(config)
+        self.last_config = config
+        server_name = self.server_name.replace("/", "_")
+        try:
+            self.parent.emit(server_name, self.last_config)
+        except BufferError:
+            print("Got buffer error in dyn reconfig")
+
+    def __init__(self, parent, server_name):
+        self.last_config = None
+        self.parent = parent
+        self.server_name = server_name
+        self.client = dynamic_reconfigure.client.Client(server_name, timeout=30, config_callback=self.callback)
+
+class ROSActionClient:
+    """
+    Internal class to handle action clients
+    """
+
+    def __init__(self, parent, server_name, server_type):
+        self.parent = parent
+        self.server_name = server_name
+        self.server_type = server_type
+        self_type = get_type_from_name(server_type+"Action")
+        self.client = actionlib.SimpleActionClient(server_name, self_type)
+
+    def feedback_cb(self, status, msg):
+        print("Got feedback: ", str(msg))
+        msg_dict = message_converter.convert_ros_message_to_dictionary(msg)
+        self.parent.emit(self.server_name.replace("/", "_")+"_feedback", msg_dict)
+
+    def done_cb(self, status, msg):
+        print("Got result: ", str(msg))
+        msg_dict = message_converter.convert_ros_message_to_dictionary(msg)
+        self.parent.emit(self.server_name.replace("/", "_")+"_done", msg_dict)
+
+    def send_goal(self, msg_dict):
+        msg = message_converter.convert_dictionary_to_ros_message(self.server_type+"Goal", {})
+        print("Waiting for action server for 5s")
+        if not self.client.wait_for_server(rospy.Duration.from_sec(5)):
+            print("Could not connect to server, please start", self.server_name)
+            return
+        print("Connected to action server")
+        self.client.send_goal(msg, done_cb=self.done_cb, feedback_cb=self.feedback_cb, active_cb=None)
--- src/flexxros/node.py
+++ src/flexxros/node.py
@@ -1,112 +1,20 @@
 from flexx import flx
 import rospy
 from .external.rospy_message_converter.src.rospy_message_converter import message_converter
-import importlib
 import asyncio
 import threading
 import tornado.platform.asyncio as torasync
-import dynamic_reconfigure.client
-import actionlib
-
-class ROSPublisher:
-
-    def __init__(self, topic, topic_type):
-
-        self.topic_type = topic_type
-        self.type = ROSNode.get_type_from_name(topic_type)
-        self.pub = rospy.Publisher(topic, self.type, queue_size=10)
-
-class ROSSubscriber:
-
-    def _callback(self, *events):
-        topic = self.topic.replace("/", "_")
-        for ev in events:
-            self.parent.emit(topic, ev)
-
-    def callback(self, msg):
-        
-        msg_dict = message_converter.convert_ros_message_to_dictionary(msg)
-        topic = self.topic.replace("/", "_")
-        try:
-            self.parent.emit(topic, msg_dict)
-        except BufferError:
-            pass
-            #print("Caught a buffer error!")
-
-    def __init__(self, parent, topic, topic_type):
-
-        self.parent = parent
-        self.topic = topic
-        self.sub = rospy.Subscriber(topic, ROSNode.get_type_from_name(topic_type), self.callback)
-
-class ROSDynReconfig:
-
-    def set_config(self):
-        if self.last_config is not None:
-            self.client.update_configuration(self.last_config)
-
-    def callback(self, config):
-
-        print(config)
-        self.last_config = config
-        server_name = self.server_name.replace("/", "_")
-        try:
-            self.parent.emit(server_name, self.last_config)
-        except BufferError:
-            print("Got buffer error in dyn reconfig")
-
-    def __init__(self, parent, server_name):
-        self.last_config = None
-        self.parent = parent
-        self.server_name = server_name
-        self.client = dynamic_reconfigure.client.Client(server_name, timeout=30, config_callback=self.callback)
-
-class ROSActionClient:
-
-    def __init__(self, parent, server_name, server_type):
-        self.parent = parent
-        self.server_name = server_name
-        self.server_type = server_type
-        self_type = ROSNode.get_type_from_name(server_type+"Action")
-        self.client = actionlib.SimpleActionClient(server_name, self_type)
-
-    def feedback_cb(self, status, msg):
-        print("Got feedback: ", str(msg))
-        msg_dict = message_converter.convert_ros_message_to_dictionary(msg)
-        self.parent.emit(self.server_name.replace("/", "_")+"_feedback", msg_dict)
-
-    def done_cb(self, status, msg):
-        print("Got result: ", str(msg))
-        msg_dict = message_converter.convert_ros_message_to_dictionary(msg)
-        self.parent.emit(self.server_name.replace("/", "_")+"_done", msg_dict)
-
-    def send_goal(self, msg_dict):
-        msg = message_converter.convert_dictionary_to_ros_message(self.server_type+"Goal", {})
-        print("Waiting for action server for 5s")
-        if not self.client.wait_for_server(rospy.Duration.from_sec(5)):
-            print("Could not connect to server, please start", self.server_name)
-            return
-        print("Connected to action server")
-        self.client.send_goal(msg, done_cb=self.done_cb, feedback_cb=self.feedback_cb, active_cb=None)
+from flexxros.internal import ROSPublisher, ROSSubscriber, ROSActionClient, ROSDynReconfig
 
 class ROSNode(flx.PyComponent):
+    """
+    A PyComponent interface that the root node in the interface must always inherit from
+    """
 
     publishers = {}
     action_clients = {}
     reconfig_clients = []
     subscribers = []
-
-    def get_type_from_name(topic_type):
-
-        # this code comes from the mongodb_store package
-        parts = topic_type.split('/')
-        cls_string = "%s.msg._%s.%s" % (parts[0], parts[1], parts[1])
-        class_data = cls_string.split(".")
-        module_path = ".".join(class_data[:-1])
-        class_str = class_data[-1]
-        module = importlib.import_module(module_path)
-
-        return getattr(module, class_str)
 
     @flx.action
     def subscribe(self, topic, topic_type):
@@ -164,6 +72,9 @@ class ROSNode(flx.PyComponent):
         pass
 
 class ROSWidget(flx.Widget):
+    """
+    flx.Widget subclass that a widget can inherit from for easy access to ros functionality
+    """
 
     def init(self):
         pass
@@ -184,6 +95,12 @@ def start_flexx():
     flx.start()
 
 def init_and_spin(node_name, app_type):
+    """
+    The main way of starting your flexxros app, takes the root 
+
+    :param node_name: the ROS node name
+    :param app_type: the root widget, must inherit from ROSNode
+    """
 
     rospy.init_node(node_name, anonymous=True)
     asyncio.set_event_loop_policy(torasync.AnyThreadEventLoopPolicy())
